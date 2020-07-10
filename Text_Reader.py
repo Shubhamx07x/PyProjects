585b4abab38c3e3f86b8177f76d73742753836fa
Shubhamx07x <67409470+Shubhamx07x@users.noreply.github.com>
2020-07-10 13:21:03 +0530

    Create Text_Reader.py
    Read the input.

---
+++ Text_Reader.py
@@ -0,0 +1,38 @@
+import pyttsx3
+import keyboard
+
+print('''
+     --------------WELCOME TO TEXT TO SPEACH--------------
+      --------------─────█─▄▀█──█▀▄─█─────--------------
+                    ────▐▌──────────▐▌────
+                    ────█▌▀▄──▄▄──▄▀▐█────
+                    ───▐██──▀▀──▀▀──██▌───
+                    ──▄████▄──▐▌──▄████▄──
+     -----------------------------------------------------
+''')
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[int(input('Choose your assistant\n1:ZIRA\n2:DAVID\nEnter: '))-1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+print('Commands\n1.To Read\n2.To Quit')
+while True:
+    x=int(input('Commands\n1.To Read\n2.To Quit\nEnter: '))
+    if x==1:
+        y=int(input('1.Enter a File path to read\n2.Enter text to read\nEnter: '))
+        if y==1:
+            fh=open(input('Enter path: ')).read()
+            print(fh)
+            speak(fh)
+                
+        elif y==2:
+            t=input('Enter text:\n')
+            speak(t)
+            if keyboard.is_pressed('Esc'):
+                engine.stop()
+    else:
+        break
+
